708b1d953639db561a298f26bec0e58c40169fdc
ucupkecil <dhitapinan@gmail.com>
2023-02-17 07:43:13 +0700

    250

--- resources/views/components/layouts/FE/index/hero.blade.php
+++ resources/views/components/layouts/FE/index/hero.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="caption-content text-center">
            
-            <h2 style="color:#ed1e52;font-size:400%;">MENGHASILKAN JUTAAN RUPIAH</h2>
+            <h2 style="color:#ed1e52;font-size:250%;">MENGHASILKAN JUTAAN RUPIAH</h2>
             <h2 style="color:white;">MELALUI TIKTOK</h2>
             
             <ul>
