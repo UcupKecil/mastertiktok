3a3b697ee4c6b895a78f198c0f5bb5c980b6f4a3
ucupkecil <dhitapinan@gmail.com>
2023-02-17 07:14:27 +0700

    0

--- resources/views/components/layouts/FE/index/head.blade.php
+++ resources/views/components/layouts/FE/index/head.blade.php
@@ -19,6 +19,7 @@
     <!--animate css-->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/templates/omexo/assets/css/animate.css') }}">
     <!--main css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/templates/omexo/assets/css/stylecuriculum0.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/templates/omexo/assets/css/style.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/templates/omexo/assets/css/stylecuriculum4.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/templates/omexo/assets/css/stylecuriculum3.css') }}">
